5b50d1875dd12906f4d51d26ecf48848c2093a68
HarmoGlace <harmoglacehelp@gmail.com>
2020-05-10 12:00:28 +0200

    Adding a few teams

--- structure/teams.js
+++ structure/teams.js
@@ -6,10 +6,22 @@ const teams = [
         aliases: ['empire'], // Team name Aliases
         subTeams: [
             {
-                id: 'sousteam1', // SubTeam ID
-                name: 'Tres Belle team', // SubTeam name
-                role: '701108549484281896', // SubTeam role ID
-                aliases: [''], // SubTeam name Aliases
+                id: 'alice', // SubTeam ID
+                name: 'Escouade Alice', // SubTeam name
+                role: '701511190739812452', // SubTeam role ID
+                aliases: ['alice'], // SubTeam name Aliases
+            },
+            {
+                id: 'bercouli',
+                name: 'Escouade Bercouli',
+                role: '701510879107219466',
+                aliases: ['bercouli']
+            },
+            {
+                id: 'fanatio',
+                name: 'Escouade Fanatio',
+                role: '701510923520704603',
+                aliases: ['fanatio']
             }
         ]
     },
@@ -24,6 +36,18 @@ const teams = [
                 name: 'Pugilistes', // SubTeam name
                 role: '701513955906027581', // SubTeam role ID
                 aliases: [''], // SubTeam name Aliases
+            },
+            {
+                id: 'gobelins',
+                name: 'Gobelins',
+                role: '701513855179685950',
+                aliases: ['gobelin']
+            },
+            {
+                id: 'assassins',
+                name: 'Assassins',
+                role: '701513979704377403',
+                aliases: ['assasin', 'asasin']
             }
         ]
     }
